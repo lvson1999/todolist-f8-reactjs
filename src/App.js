a272d139c96f0bd4a4357b8f811d144d040fd148
Son Le <levanson01121999@gmail.com>
2023-02-20 17:04:37 +0700

    update todolist ff8

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  
+  const [job, setJob] = useState('');
+  const [jobs, setJobs] = useState(() => {
+    const storageJobs = JSON.parse(localStorage.getItem('jobs'))
+    return storageJobs;
+  });
+
+
+  const handleSubmit = () => {
+    setJobs(prev => {
+      const newJobs = [...prev, job]
+
+      const jsonJobs = JSON.stringify(newJobs)
+      localStorage.setItem('jobs', jsonJobs)
+
+      return newJobs
+    })
+
+    setJob('')
+  }
+
   return (
-    <div className="App">
-      Nhật Ngu
+    <div style={{ padding: 32}}>
+      <input 
+      value={job} 
+      onChange={e => setJob(e.target.value)}
+      />
+      <button onClick={handleSubmit}>Add</button>
+
+      <ul>
+        {jobs.map((job, index) => (
+          <li key={index}>{job}</li>
+        ))}
+        
+      </ul>
     </div>
   );
 }
